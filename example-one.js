8151c9fc650759e8eb544e8ce86527d079ee7949
David Roberts <david.roberts@concentra.co.uk>
2019-10-19 15:30:10 +0100

    Inital commit

---
+++ example-one.js
@@ -0,0 +1,30 @@
+const myFunction = (...args) => {
+    console.log('myFunction()')
+    console.log('this is the argument length', args.length)
+    console.log('these are the arguments (spread)', ...args)
+    console.log('these are the arguments (args)', args)
+}
+
+const mySecondFunction = (arg1, arg2, arg3, arg4) => {
+    console.log('mySecondFunction()')
+    console.log('arg1', arg1)
+    console.log('arg2', arg2)
+    console.log('arg3', arg3)
+    console.log('arg4', arg4)
+}
+
+// example one
+myFunction([1,2,3,4])
+
+// exmaple two
+myFunction('1','2', '3', '4')
+
+// exmaple three
+myFunction('one')
+
+// example four
+myFunction()
+
+//exmaple five
+const args = [1,2,3]
+mySecondFunction(...args)
